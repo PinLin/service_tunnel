163afab2ca77b779d15b8c74cc43c5855fd3ba7c
Pin Lin <moneycat711@gmail.com>
2019-01-20 20:57:58 +0800

    Fix bug of ssh username

--- service_tunnel.sh
+++ service_tunnel.sh
@@ -2,7 +2,7 @@
 
 while true; do
     # CHIP SSH
-    ssh -o "ExitOnForwardFailure yes" -NR 20023:localhost:22 ntut.com.tw
+    ssh -o "ExitOnForwardFailure yes" -NR 20023:localhost:22 pinlin@ntut.com.tw
 done &
 
 while true; do
