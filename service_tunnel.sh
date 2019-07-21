a85dcc10213023182d28cbaec7f4124acb4e47c4
Pin Lin <moneycat711@gmail.com>
2019-07-22 04:00:20 +0800

    Switch ntut.com.tw to tunnel.ntut.com.tw

--- service_tunnel.sh
+++ service_tunnel.sh
@@ -2,12 +2,12 @@
 
 while true; do
     # CHIP SSH
-    ssh -o "ExitOnForwardFailure yes" -NR 20023:localhost:22 pinlin@ntut.com.tw
+    ssh -o "ExitOnForwardFailure yes" -NR 20023:localhost:22 pinlin@tunnel.ntut.com.tw
 done &
 
 while true; do
     # CHIP Web
-    ssh -o "ExitOnForwardFailure yes" -NR 20080:localhost:80 pinlin@ntut.com.tw
+    ssh -o "ExitOnForwardFailure yes" -NR 20080:localhost:80 pinlin@tunnel.ntut.com.tw
 done &
 
 while true; do
