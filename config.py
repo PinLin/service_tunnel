10ebe93c6cc9ecc70e96e801daab3ba8de6d60db
Pin Lin <moneycat711@gmail.com>
2020-01-24 00:18:05 +0800

    Reset to example config

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
 TUNNELS = [
     {
-        'description': "CHIP SSH",
-        'user': 'chip',
-        'address': ':20022:localhost:22',
-        'destination': 'pinlin@tunnel.ntut.com.tw',
-    },
-    {
-        'description': "CHIP Web",
-        'user': 'chip',
-        'address': '20080:localhost:80',
-        'destination': 'pinlin@tunnel.ntut.com.tw',
+        'description': "PC01 MariaDB",
+        'user': 'sa',
+        'address': ':3306:localhost:3306',
+        'destination': 'sa@example.com',
     },
 ]
