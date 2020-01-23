5f91b14198f1454e12c8d7d22f5aeed216c60311
Pin Lin <moneycat711@gmail.com>
2020-01-24 00:13:49 +0800

    Reengineer

---
+++ config.py
@@ -0,0 +1,14 @@
+TUNNELS = [
+    {
+        'description': "CHIP SSH",
+        'user': 'chip',
+        'address': ':20022:localhost:22',
+        'destination': 'pinlin@tunnel.ntut.com.tw',
+    },
+    {
+        'description': "CHIP Web",
+        'user': 'chip',
+        'address': '20080:localhost:80',
+        'destination': 'pinlin@tunnel.ntut.com.tw',
+    },
+]
